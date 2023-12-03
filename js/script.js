6b27eee4b6908ac82368467616407abe0b79430d
devdaniellle <devdaniellle@gmail.com>
2023-12-03 19:09:35 +0500

    finished header(navbar) features

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// on/off navbar(header) menu
+
 let menu = document.querySelector('#menu')
 let navbar = document.querySelector('.header .menu')
 
@@ -8,3 +10,23 @@ menu.onclick = () => {
 let changeIcon = function (icon) {
   icon.classList.toggle('fa-times')
 }
+
+// show/hide navbar(header) menu
+
+const nav = document.getElementById('navbar')
+let main = document.getElementById('major-block')
+var mainHeight = main.offsetHeight
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > mainHeight*0.85) {
+    nav.classList.add('active')
+  } else {
+    nav.classList.remove('active')
+  }
+})
+
+// hide navbar(header) after click on each element
+
+navbar.addEventListener('click', () => {
+  navbar.classList.remove('active')
+})
